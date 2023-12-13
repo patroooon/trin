1aaf0344989e1e8c5d2fd1eb1d2112c70b1eddd3
Pavel Home <kapabahone@gmail.com>
2023-12-13 12:46:07 +0300

    Score2

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -14,16 +14,20 @@ namespace TrafficInfinity
         {
             rb = GetComponent<Rigidbody>();
             oldPosition = rb.position;
+
+            InvokeRepeating("CalculateDistance", 1f, 1f);
         }
 
-        private void Update()
+        private void CalculateDistance()
         {
             Vector3 newPosition = rb.position;
             float distance = Vector3.Distance(newPosition, oldPosition);
             distanceTraveled += distance;
             oldPosition = newPosition;
 
-            Debug.Log("Пройденное расстояние: " + distanceTraveled);
+            int roundedDistance = Mathf.RoundToInt(distanceTraveled);
+            Debug.Log("Пройденное расстояние: " + roundedDistance);
         }
     }
+    
 }
