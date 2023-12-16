42c4465d6cb7538585e01f61c4fae0473fdb4383
Pavel Home <kapabahone@gmail.com>
2023-12-16 22:07:46 +0300

    UI2

--- Assets/Scripts/GameEvent.cs
+++ Assets/Scripts/GameEvent.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 namespace TrafficInfinity
 {
     public static class GameEvent
-    {
-        public static event System.Action onMoveCar;
+    {       
         public static event System.Action onFar;
         public static event System.Action onCrash;
 
