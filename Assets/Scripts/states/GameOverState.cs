e9fcd8b1ba354c0e8ed0b7f7d7de1cf5816b7861
Pavel Home <kapabahone@gmail.com>
2024-01-06 15:31:32 +0300

    Level Refresh, TCT

--- Assets/Scripts/states/GameOverState.cs
+++ Assets/Scripts/states/GameOverState.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 namespace TrafficInfinity
 {
@@ -17,9 +18,10 @@ namespace TrafficInfinity
         {
             Exit();
             mainMenuState.Enter();
-            Van.transform.position = new Vector3(0, 0, 0);
-            Player.transform.position = new Vector3(0, 0, 0);
+            //Van.transform.position = new Vector3(0, 0, 0);
+            //Player.transform.position = new Vector3(0, 0, 0);
             Van.SetActive(false);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
