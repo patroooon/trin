31b91f7e611f86016f1af78ecc463297d7cd305d
Pavel Home <kapabahone@gmail.com>
2024-01-17 20:20:08 +0300

    Content1Step

--- Assets/Scripts/StreetLightCreat.cs
+++ Assets/Scripts/StreetLightCreat.cs
@@ -6,6 +6,24 @@ namespace TrafficInfinity
 {
     public class StreetLightCreat : MonoBehaviour
     {
+        /* public Transform[] objectsToPosition;
+         public float distanceBetweenObjects = 40f;
+
+         private void PositionObjects()
+         {
+             float startZ = 0f;
+
+             foreach (Transform obj in objectsToPosition)
+             {
+                 obj.position = new Vector3(obj.position.x, obj.position.y, startZ);
+                 startZ += distanceBetweenObjects;
+             }
+         }
+        */
+
+
+
+
         public GameObject prefab;
         void Start()
         {
@@ -13,6 +31,8 @@ namespace TrafficInfinity
             {
                 Instantiate(prefab, new Vector3(i * 0, 0, 40.0f), Quaternion.identity);
             }
+
         }
     }
+
 }
